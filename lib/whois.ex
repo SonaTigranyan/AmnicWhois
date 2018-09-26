4c885b4b13521455842ec6bb8cbf320186462f1c
Sona Tigranyan <sonatigranyan1998@gmail.com>
2018-09-26 12:41:18 +0400

    finalized recv and handler to check for available domains

--- lib/whois.ex
+++ lib/whois.ex
@@ -1,25 +1,41 @@
 defmodule Whois do
   require Logger
 
-  def connect(domain_name) do
+
+  def is_avail? (domain_name) do
+    do_connect(domain_name)
+  end
+
+  def do_connect(domain_name) do
     host = 'whois.amnic.net'
     case :gen_tcp.connect(host, 43, [packet: 0, active: false]) do
       {:ok, socket} ->
-        send(socket, domain_name)
+        do_send(socket, domain_name)
       {:error, errno} ->
-        Logger.error "connction error"
+        Logger.error errno
     end
   end
 
-  def send(socket, domain_name) do
-    case :gen_tcp.send(socket, domain_name) do
-      {:ok, socket} ->
-
-      {:error, errno} ->
-        Logger.error "send error"
+  def do_send(socket, domain_name) do
+    case :gen_tcp.send(socket, domain_name ++ '\r\n') do
+      :ok ->
+        do_recv(socket, 0)
+      {:error, errno} -> 
+        Logger.error errno
       end
   end
 
-  def recv() do
+  def do_recv(socket, length) do
+    case :gen_tcp.recv(socket, length) do
+      {:ok, data} -> 
+        data_handler(data)
+      {:error, errno} -> 
+        Logger.error errno
+    end
   end
+
+  def data_handler(data) do
+    data |> to_string |> String.ends_with?("\n\nNo match\n\n")
+  end
+
 end
